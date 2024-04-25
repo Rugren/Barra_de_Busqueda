e04f65968b1063acd57413883a9bedcfacc92dde
Rugren <rubgarciaruiz@gmail.com>
2024-04-25 09:36:59 +0200

    Creada estructura de carpetas + Componente Seach
    - Creada estructura de carpetas
    - Creado Componente seach: ng g c components/search (para que nos lo cree dentro de la carpeta components)

--- Search/src/app/app.module.ts
+++ Search/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
